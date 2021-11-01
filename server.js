9d80ca988d9abcefc6ea8a27a554a393a59297d2
Jose Mejia <jose.mejia@e-volution.co>
2021-11-01 13:21:52 -0500

    added express

---
+++ server.js
@@ -0,0 +1,24 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+// leanTechTest
+const express = require('express');
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/lean-tech-test'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/lean-tech-test/'}),
+);
+
+// app.listen(process.env.PORT || 8080);
+port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log('Server is on port ', port);
+});
